2ce907ffba216cddd35d5f009906f45789b608ac
Welby Seely <welbyseely@gmail.com>
2024-09-10 01:16:17 -0400

    Can learn OR and AND functions

--- exercises.py
+++ exercises.py
@@ -1,7 +1,5 @@
-import math, numpy
+import numpy as np
 from math import exp
-
-from numpy import asarray, dot, zeros
 
 
 def relu(x: float):
@@ -19,24 +17,64 @@ def step_function(x: float) -> float:
 
 
 def initialize_perceptron(weights, bias, activation):
-    def process(inputs):
-        return activation(dot(weights, inputs) + bias), weights, bias
-    return process
+    def forward_pass(inputs):
+        return activation(np.dot(weights, inputs) + bias), weights, bias
 
-def train_perceptron(initial_perceptron, epochs, cases):
-    r = 0.1
-    inputs = []
-    # for case in cases:
-    #     inputs.append(arr[:-1])
+    return forward_pass
 
 
+def train_perceptron(activation, cases, epochs, learning_rate):
+    perceptron = initialize_perceptron(np.zeros(2), 0, activation)
+    case_arrays = [np.array(case) for case in cases]
+    for epoch in range(epochs):
+        for case in case_arrays:
+            inputs = case[:-1]
+            expected = case[-1]
+            actual, weights, bias = perceptron(inputs)
+            learning_coefficient = learning_rate * (expected - actual)
+            adjusted_weights = np.add(weights, np.multiply(inputs, learning_coefficient))
+            adjusted_bias = bias + learning_coefficient
+            # print(f"epoch={epoch}, inputs={inputs}, actual={actual}, expected={expected}, weights={weights}, "
+            #       f"bias={bias}, learning_coefficient={learning_coefficient}, "
+            #       f"adjusted_weights={adjusted_weights}, adjusted_bias={adjusted_bias}")
+            perceptron = initialize_perceptron(adjusted_weights, adjusted_bias, activation)
+    return perceptron
 
+
+def and_truth_table():
+    return [
+        [0, 0, 0],
+        [1, 0, 0],
+        [0, 1, 0],
+        [1, 1, 1],
+    ]
+
+
+def or_truth_table():
+    return [
+        [0, 0, 0],
+        [0, 1, 1],
+        [1, 0, 1],
+        [1, 1, 1]
+    ]
+
+
+def xor_truth_table():
+    return [
+        [0, 0, 0],
+        [1, 0, 1],
+        [0, 1, 1],
+        [1, 1, 0],
+    ]
+
+
+def train():
+    # or_perceptron = train_perceptron(step_function, or_truth_table(), 10, 0.3)
+    and_perceptron = train_perceptron(relu, and_truth_table(), 10000, 0.1)
+    # xor_perceptron = train_perceptron(step_function, xor_truth_table(), 10, 0.1)
+
+    print(and_perceptron(np.array([1, 1]))[0])
 
 
 if __name__ == '__main__':
-    init_weights = zeros(2)
-    init_bias = 0
-    forward_pass = initialize_perceptron(init_weights, init_bias, step_function)
-    result, weights, bias = forward_pass(asarray([0, 1]))
-    print(f"result={result}, weights={weights}, bias={bias}")
-    cases = [[0,0,0],[0,1,1],[1,0,1],[1,1,1]]
+    train()
